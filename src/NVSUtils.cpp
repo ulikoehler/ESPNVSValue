41559c6429af4b5175d32f5de13733b96826e750
Uli Köhler <ukoehler@techoverflow.net>
2024-02-01 21:48:35 +0100

    Fix NVSUtils.cpp key variable bug

--- src/NVSUtils.cpp
+++ src/NVSUtils.cpp
@@ -7,10 +7,10 @@ NVSQueryResult NVSValueSize(nvs_handle_t nvs, const std::string& key, size_t& si
     if((err = nvs_get_blob(nvs, key.c_str(), nullptr, &size)) != ESP_OK) {
         if(err == ESP_ERR_NVS_NOT_FOUND) {
             // Not found, no error
-            NVSPrintf(NVSLogLevel::Debug, "Key %s does not exist", _key.c_str());
+            NVSPrintf(NVSLogLevel::Debug, "Key %s does not exist", key.c_str());
             return NVSQueryResult::NotFound;
         } else {
-            NVSPrintf(NVSLogLevel::Error, "Failed to get size of NVS key %s: %s", _key.c_str(), esp_err_to_name(err));
+            NVSPrintf(NVSLogLevel::Error, "Failed to get size of NVS key %s: %s", key.c_str(), esp_err_to_name(err));
             return NVSQueryResult::Error;
         }
     }
