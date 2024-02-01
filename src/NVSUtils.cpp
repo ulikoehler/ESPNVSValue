4071df66f111a5f551084e2bc5d434b7ce789338
Uli Köhler <ukoehler@techoverflow.net>
2024-02-01 21:41:17 +0100

    Fix NVSValueSize function to return appropriate NVSQueryResult

--- src/NVSUtils.cpp
+++ src/NVSUtils.cpp
@@ -7,12 +7,12 @@ NVSQueryResult NVSValueSize(nvs_handle_t nvs, const std::string& key, size_t& si
     if((err = nvs_get_blob(nvs, key.c_str(), nullptr, &size)) != ESP_OK) {
         if(err == ESP_ERR_NVS_NOT_FOUND) {
             // Not found, no error
-            _exists = false;
-            _value = "";
             NVSPrintf(NVSLogLevel::Debug, "Key %s does not exist", _key.c_str());
+            return NVSQueryResult::NotFound;
         } else {
             NVSPrintf(NVSLogLevel::Error, "Failed to get size of NVS key %s: %s", _key.c_str(), esp_err_to_name(err));
-            return;
+            return NVSQueryResult::Error;
         }
     }
+    return NVSQueryResult::OK;
 }
